c9fec5eb16cf27972fd19881f3bc90787476af66
shresthanikhil16 <shresthanikhil16@gmail.com>
2025-01-27 20:47:37 +0545

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { lazy } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import DashboardPage from "./core/public/dashboard";
+import ForgetPassword from "./core/public/forgetPassword";
+import GetStarted from "./core/public/gettingstarted";
+import ResetPasswordPage from "./core/public/resetPassword";
 import SignupPage from "./core/public/signup";
 
-// import SignupPage directly as it's already being used.
-
-const Dashboard = lazy(() => import("./core/public/dashboard"));
+// Lazy-load all components
+const DashboardPage = lazy(() => import("./core/public/dashboard"));
+const AdminDashboardPage = lazy(() => import("./core/public/admindashboard"));
+const WelcomePage = lazy(() => import("./core/public/welcomepage"));
 const Login = lazy(() => import("./core/public/login"));
 const Register = lazy(() => import("./core/public/signup"));
 const Layout = lazy(() => import("./core/private/layout"));
 
 function App() {
   const publicRoutes = [
-    { path: "/", element: <DashboardPage /> },
+    { path: "/", element: <GetStarted /> },
+    { path: "/welcomepage", element: <WelcomePage /> },
     { path: "/login", element: <Login /> },
     { path: "/signup", element: <SignupPage /> },
-    { path: "*", element: <>unauthorized</> },
+    { path: "/dashboard", element: <DashboardPage /> },
+    { path: "/admindashboard", element: <AdminDashboardPage /> },
+    { path: "/forgetpassword", element: <ForgetPassword /> },
+    { path: "/resetpassword", element: <ResetPasswordPage /> },
+    { path: "*", element: <>Unauthorized</> },
   ];
 
   const privateRoutes = [
@@ -24,8 +32,9 @@ function App() {
   ];
 
   // LOGIN logic TODO
-  const isAdmin = false;
+  const isAdmin = false; // Replace with actual logic for admin status
   const routes = isAdmin ? privateRoutes : publicRoutes;
+
   return (
     <>
       <RouterProvider router={createBrowserRouter(routes)} />
